4da3395ddaecf5e74e139b12f55ef604f6ab05e3
actualfyrn <actual@fyrn.wtf>
2025-12-22 22:36:59 -0600

    Updating main.rs with future mod comment, other comment changes.

--- aviary-main/src/main.rs
+++ aviary-main/src/main.rs
@@ -1,6 +1,10 @@
 // src/wgpu_render.rs
-// Basic window rendering with update and input handling.
+// Handles base window rendering with update and input handling.
 mod wgpu_render;
+
+// src/audio_render.rs
+// Handles base audio rendering and handling before playback.
+// mod audio_render;
 
 fn main() {
     // Start rendering in new window, begin wait for updates and input.
